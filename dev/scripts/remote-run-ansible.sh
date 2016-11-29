40bb02ef0f87be12ff15dffb1785cfff9c0745d2
Frederic Arpin <frederic.arpin@fxinnovation.com>
2016-11-29 13:38:46 -0500

    2016-11-29: Waiting on BN for answers

--- dev/scripts/remote-run-ansible.sh
+++ dev/scripts/remote-run-ansible.sh
@@ -10,6 +10,8 @@ hostinfo="${1}"
 
 ./remote-install-ansible.sh ${hostinfo}
 
-scp "${SCRIPTDIR}/../ansible/hardening-RH-7.2.yml" ${hostinfo}:
-ssh -t ${hostinfo} sudo ansible-playbook hardening-RH-7.2.yml
+#scp "${SCRIPTDIR}/../ansible/hardening-RH-7.2.yml" ${hostinfo}:
+ssh -t ${hostinfo} rm -rf ansible/
+scp -r "${SCRIPTDIR}/../ansible" ${hostinfo}:
+ssh -t ${hostinfo} "cd ansible; sudo ansible-playbook hardening-RH-7.2.yml"
 
